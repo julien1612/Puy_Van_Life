c15feaf81e0aa8e77d6d8e1889565530660679f1
julien1612 <cjulien1612@gmail.com>
2025-07-29 22:37:11 +0200

    charterOk

--- src/Form/CommentForm.php
+++ src/Form/CommentForm.php
@@ -5,19 +5,29 @@ namespace App\Form;
 use App\Entity\Comment;
 use App\Entity\Location;
 use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 class CommentForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('rating')
-            ->add('comment');
+            ->add('rating', IntegerType::class, [
+                'constraints' => [
+                    new Range(['min' => 0, 'max' => 10])
+                ],
+                'attr' => ['step' => 0.5, 'min' => 0, 'max' => 10]
 
+            ])
+            ->add('comment');
 
 
     }
